1b09a628e0179db7f576236f825e34ca42443912
anshitaagrawal09 <anshitaagrawal75@gmail.com>
2025-03-01 23:13:26 +0530

    UC-2

--- src/main/java/com/example/springconcept/HelloController.java
+++ src/main/java/com/example/springconcept/HelloController.java
@@ -1,13 +1,28 @@
 package com.example.springconcept;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        return "hello"; // This will look for hello.html in src/main/resources/templates/
     }
 }
+
+//UC-1
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloController {
+//
+//    @GetMapping("/hello")
+//    public String sayHello() {
+//        return "Hello from BridgeLabz";
+//    }
+//}
